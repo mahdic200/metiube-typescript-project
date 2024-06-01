d3f71a2ddcc6654ea7a7773df369c6aea955735a
mahdic200 <rmahdi170@gmail.com>
2024-06-01 19:48:39 +0330

    updated Run.py tool

--- Run.py
+++ Run.py
@@ -4,13 +4,13 @@ Tail_output_file = "./build/styles/index.css"
 
 # TypeScript configurations
 TSC_input_file = "./src/ts/index.ts"
-TSC_output_directory = "./build/js"
+TSC_output_directory = "./build/js/category/"
 
 # Ejs configurations
 Ejs_input_directory = "./src/pages"
 Ejs_input_file = "./src/pages/index.ejs"
 Ejs_output_file = "./build/index.html"
-Ejs_interval = 1
+Ejs_interval = 0.1
 
 
 import os
@@ -24,6 +24,7 @@ def get_files(directory):
     files = {}
     for filename in os.listdir(directory):
         filepath = os.path.join(directory, filename)
+        filepath = provide_file_path(filepath)
         if os.path.isfile(filepath):
             files[filepath] = os.path.getmtime(filepath)
         elif os.path.isdir(filepath):
@@ -61,19 +62,72 @@ def watch_directory(directory, action, interval=1):
             action(added_or_modified, deleted)
             previous_files = current_files
 
-def on_change(added_or_modified, deleted):
-    """
-    Action to perform when changes are detected.
+def provide_file_path(path):
+    path = path.replace("/", os.sep)
+    path = path.replace("\\", os.sep)
+    return path
+
+def path_equivalent(filepath):
+    pages_path = provide_file_path("./src/pages")
+    build_path = provide_file_path("./build")
+    new_path = filepath.replace(pages_path, "")
+    new_path = new_path.replace("ejs", "html")
+    new_path = build_path + new_path
+    return new_path
+
+def is_layout(path):
+    if "layouts" not in path:
+        return False
+    elif "components" not in path:
+        return False
+    else:
+        return True
+
+def generateEJS(inp_file, out_file):
+    out_dir = os.path.dirname(out_file)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    system(f"ejs {inp_file} -o {out_file}")
+
+def generate_Ejs_files(list_or_dict):
+    if not isinstance(list_or_dict, dict) and not isinstance(list_or_dict, list):
+        raise Exception("generate_Ejs_files func : Bad argument !")
+    for filepath in list_or_dict:
+        if not is_layout(filepath):
+            new_path = path_equivalent(filepath)
+            generateEJS(filepath, new_path)
+
+def delete_html(filepath):
+    out_dir = os.path.dirname(filepath)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        if not os.listdir(out_dir):
+            os.rmdir(out_dir)
+
+def delete_html_files(list_or_dict):
+    if not isinstance(list_or_dict, dict) and not isinstance(list_or_dict, list):
+        raise Exception("delete_html_files func : Bad argument !")
+    for filepath in list_or_dict:
+        if not is_layout(filepath):
+            eq_path = path_equivalent(filepath)
+            delete_html(eq_path)
     
-    Parameters:
-        added_or_modified (list): List of added or modified file paths.
-        deleted (list): List of deleted file paths.
-    """
-    system(f"ejs {Ejs_input_file} -o {Ejs_output_file}")
+
+def on_change(added_or_modified, deleted):
     if added_or_modified:
         print(f"Ejs : Added or modified: {added_or_modified}")
+        generate_all = False
+        for filepath in added_or_modified:
+            if is_layout(filepath):
+                generate_all = True
+                break
+        if generate_all:
+            generate_Ejs_files(get_files(Ejs_input_directory))
+        else:
+            generate_Ejs_files(added_or_modified)
     if deleted:
         print(f"Deleted: {deleted}")
+        delete_html_files(deleted)
 
 def watch_file(filepath, interval=1):
 
@@ -88,7 +142,7 @@ def watch_file(filepath, interval=1):
         
         if current_modified_time != last_modified_time:
             print(f"Ejs : The file '{filepath}' has been modified.")
-            system(f"ejs {Ejs_input_file} -o {Ejs_output_file}")
+            generateEJS(Ejs_input_file, Ejs_output_file)
             last_modified_time = current_modified_time
 
 
